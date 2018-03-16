adae6ba1e2ed2b415fecfedc54f5116bfa5f16b4
andriy-sa <andriy0smolyar0@selectoglobal.com>
2018-03-16 18:34:57 +0200

    init

---
+++ search/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField(default='')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
---
+++ search/search_indexes.py
@@ -0,0 +1,18 @@
+from haystack import indexes
+from search.models import Product
+
+
+class NoteIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    name = indexes.CharField(model_attr='name', boost=1.125)
+    price = indexes.DecimalField(model_attr='price')
+    category = indexes.CharField(model_attr='category')
+    description = indexes.CharField(model_attr='description')
+    created_at = indexes.DateTimeField(model_attr='created_at')
+
+    def get_model(self):
+        return Product
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
